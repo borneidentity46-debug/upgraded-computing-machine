307b76a5d2bb7fbacf40b5473bf50f41067c05d8
borneidentity46-debug <borneidentity46@gmail.com>
2025-11-13 05:24:14 +0000

    Add files via upload

---
+++ WanderWisePortal.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Send, FileText, User, Award, Briefcase, Quote } from "lucide-react";
+
+export default function WanderWisePortal() {
+  return <div className="text-center p-10">WanderWise Portal Ready</div>;
+}
